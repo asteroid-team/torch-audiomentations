6cc8614622964fcc6f3057b1d97cf54101217e8e
iver56 <ij@aiascience.com>
2020-09-21 15:33:46 +0200

    Explain how to release a new version of the package

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-        "Development Status :: 1 - Planning",
+        "Development Status :: 2 - Pre-Alpha",
         "Intended Audience :: Developers",
         "Intended Audience :: Science/Research",
         "Topic :: Multimedia",
