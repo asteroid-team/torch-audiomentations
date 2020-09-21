7ba9d2345ff25936f2295a43407464af9a648a43
iver56 <ij@aiascience.com>
2020-09-21 15:02:29 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,51 @@
+import codecs
+import os
+import re
+
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as readme_file:
+    long_description = readme_file.read()
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+
+def read(*parts):
+    with codecs.open(os.path.join(here, *parts), "r") as fp:
+        return fp.read()
+
+
+def find_version(*file_paths):
+    version_file = read(*file_paths)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
+
+setup(
+    name="torch-audiomentations",
+    version=find_version("torch_audiomentations", "__init__.py"),
+    author="Iver Jordal",
+    description="A Pytorch library for audio data augmentation. Inspired by audiomentations."
+    " Useful for deep learning.",
+    license="MIT",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/asteroid-team/torch-audiomentations",
+    packages=find_packages(exclude=["demo", "test_fixtures", "tests"]),
+    install_requires=["torch"],
+    python_requires=">=3.6",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "Topic :: Multimedia",
+        "Topic :: Multimedia :: Sound/Audio",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+    ],
+)
