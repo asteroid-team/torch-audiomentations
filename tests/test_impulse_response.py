f33438aeed5597fbf716088837a001a3a17a8ba5
Francis Lata <francisalbertlata@gmail.com>
2020-09-24 13:05:53 +0000

    add test for zero length input signals

--- tests/test_impulse_response.py
+++ tests/test_impulse_response.py
@@ -9,6 +9,7 @@ class TestApplyImpulseResponse(unittest.TestCase):
     def setUp(self):
         self.sample_rate = 16000
         self.batch_size = 32
+        self.empty_input_audio = torch.empty(0)
         self.input_audio = torch.from_numpy(load_audio(os.path.join(TEST_FIXTURES_DIR, "acoustic_guitar_0.wav"), self.sample_rate))
         self.input_audios = torch.stack([self.input_audio] * self.batch_size)
         self.ir_path = os.path.join(TEST_FIXTURES_DIR, "ir")
@@ -32,3 +33,7 @@ class TestApplyImpulseResponse(unittest.TestCase):
         mixed_inputs = self.ir_transform_no_guarantee(self.input_audios, self.sample_rate)
         self.assertEqual(mixed_inputs.size(0), self.input_audios.size(0))
         self.assertEqual(mixed_inputs.size(-1), self.input_audios.size(-1))
+
+    def test_impulse_response_guaranteed_with_zero_length_samples(self):
+        mixed_inputs = self.ir_transform_guaranteed(self.empty_input_audio, self.sample_rate)
+        self.assertTrue(torch.equal(mixed_inputs, self.empty_input_audio))
