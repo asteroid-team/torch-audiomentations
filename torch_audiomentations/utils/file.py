96af59a2c5a9d3a84d9f5c2c95673ebcc8aaab87
mpariente <pariente.mnl@gmail.com>
2020-09-25 11:24:43 +0200

    Fix black style to pass CI

--- torch_audiomentations/utils/file.py
+++ torch_audiomentations/utils/file.py
@@ -3,7 +3,7 @@ import glob
 import librosa
 
 
-SUPPORTED_EXTENSIONS = ['.wav']
+SUPPORTED_EXTENSIONS = [".wav"]
 
 
 def find_audio_files(path):
@@ -11,9 +11,10 @@ def find_audio_files(path):
     files = []
 
     for supported_extension in SUPPORTED_EXTENSIONS:
-        files.extend(glob.glob(os.path.join(path, '*' + supported_extension)))
+        files.extend(glob.glob(os.path.join(path, "*" + supported_extension)))
 
     return files
+
 
 def load_audio(audio_file_path, sample_rate):
     """Loads the audio given the path of an audio file."""
