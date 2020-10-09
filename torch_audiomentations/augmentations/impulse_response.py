9624fce262ba23072c911658ae45a9b806bc4479
iver56 <ij@aiascience.com>
2020-10-09 15:28:18 +0200

    Slice convolved audio tensors to be compatible with the original length

--- torch_audiomentations/augmentations/impulse_response.py
+++ torch_audiomentations/augmentations/impulse_response.py
@@ -12,9 +12,7 @@ class ApplyImpulseResponse(BaseWaveformTransform):
     Convolves an input signal with an impulse response.
     """
 
-    def __init__(
-        self, ir_path, device=torch.device("cpu"), convolve_mode="full", p=0.5
-    ):
+    def __init__(self, ir_path, device=torch.device("cpu"), convolve_mode="full", p=0.5):
         # TODO: infer device from the given samples instead
         super(ApplyImpulseResponse, self).__init__(p)
 
@@ -36,14 +34,14 @@ class ApplyImpulseResponse(BaseWaveformTransform):
             ir_samples = torch.from_numpy(ir_samples)
             ir_sounds.append(ir_samples)
         for i in range(len(ir_sounds)):
-            placeholder = torch.zeros(
-                size=(max_ir_sound_length,), dtype=torch.float32
-            )
+            placeholder = torch.zeros(size=(max_ir_sound_length,), dtype=torch.float32)
             placeholder[0 : len(ir_sounds[i])] = ir_sounds[i]
             ir_sounds[i] = placeholder
         self.parameters["ir_sounds"] = torch.stack(ir_sounds)
 
     def apply_transform(self, selected_samples, sample_rate: int):
         selected_samples = selected_samples.to(self.device)
+        original_length = selected_samples.shape[-1]
         ir = self.parameters["ir_sounds"].to(self.device)
-        return convolve(selected_samples, ir, mode=self.convolve_mode)
+        convolved_samples = convolve(selected_samples, ir, mode=self.convolve_mode)
+        return convolved_samples[..., :original_length]
