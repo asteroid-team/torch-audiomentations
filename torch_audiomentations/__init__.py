76f1ae39577676f98475f948ac5bc19902bf22ee
iver56 <ij@aiascience.com>
2020-10-12 13:51:28 +0200

    Import Gain to torch_audiomentations for easier API

--- torch_audiomentations/__init__.py
+++ torch_audiomentations/__init__.py
@@ -1,10 +1,11 @@
-from .augmentations.polarity_inversion import PolarityInversion
-from .augmentations.impulse_response import ApplyImpulseResponse
 from .augmentations.background_noise import ApplyBackgroundNoise
+from .augmentations.impulse_response import ApplyImpulseResponse
+from .augmentations.gain import Gain
+from .augmentations.polarity_inversion import PolarityInversion
 
 # TODO: Revise these imports
 from .utils.convolution import convolve
-from .utils.file import find_audio_files, load_audio
 from .utils.dsp import calculate_rms, calculate_desired_noise_rms, resample_audio
+from .utils.file import find_audio_files, load_audio
 
 __version__ = "0.0.1"
