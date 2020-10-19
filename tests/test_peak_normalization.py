803f06c6c242b709d3fa60bd03dc5e0149f30639
iver56 <ij@aiascience.com>
2020-10-19 11:23:05 +0200

    Test stereo sound in PeakNormalization

--- tests/test_peak_normalization.py
+++ tests/test_peak_normalization.py
@@ -234,3 +234,41 @@ class TestPeakNormalization(unittest.TestCase):
             ),
         )
         self.assertEqual(processed_samples2.dtype, np.float32)
+
+    def test_stereo_sound(self):
+        samples = np.array(
+            [
+                [[0.75, 0.5, -0.25, -0.125, 0.0], [0.65, 0.5, -0.25, -0.125, 0.0]],
+                [[0.3, 0.5, -0.25, -0.125, 0.0], [0.9, 0.5, -0.25, -0.125, 0.0]],
+                [[0.9, 0.5, -0.25, -1.06, 0.0], [0.9, 0.5, -0.25, -1.12, 0.0]],
+            ],
+            dtype=np.float32,
+        )
+        sample_rate = 16000
+
+        augment = PeakNormalization(apply_to="all", p=1.0)
+        processed_samples = augment(
+            samples=torch.from_numpy(samples), sample_rate=sample_rate
+        ).numpy()
+
+        assert_almost_equal(
+            processed_samples,
+            np.array(
+                [
+                    np.array(
+                        [[0.75, 0.5, -0.25, -0.125, 0.0], [0.65, 0.5, -0.25, -0.125, 0.0]]
+                    )
+                    / 0.75,
+                    np.array(
+                        [[0.3, 0.5, -0.25, -0.125, 0.0], [0.9, 0.5, -0.25, -0.125, 0.0]]
+                    )
+                    / 0.9,
+                    np.array(
+                        [[0.9, 0.5, -0.25, -1.06, 0.0], [0.9, 0.5, -0.25, -1.12, 0.0]]
+                    )
+                    / 1.12,
+                ],
+                dtype=np.float32,
+            ),
+        )
+        self.assertEqual(processed_samples.dtype, np.float32)
