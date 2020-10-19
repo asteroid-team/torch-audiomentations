a197d8e80027eae6496ebf8c7421a28f8a04244e
iver56 <ij@aiascience.com>
2020-10-19 11:24:09 +0200

    Expose PeakNormalization and convolve to the public API

--- torch_audiomentations/__init__.py
+++ torch_audiomentations/__init__.py
@@ -1,4 +1,7 @@
 from .augmentations.gain import Gain
 from .augmentations.polarity_inversion import PolarityInversion
+from .augmentations.peak_normalization import PeakNormalization
+
+from .utils.convolution import convolve
 
 __version__ = "0.1.0"
