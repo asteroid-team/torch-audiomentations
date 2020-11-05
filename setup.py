b5eb9e18ff704c098e5c9995e502673cc456489d
iver56 <ij@aiascience.com>
2020-11-05 15:06:12 +0100

    Reduce dependency spec confusion by removing tests_require. Close #33

--- setup.py
+++ setup.py
@@ -37,7 +37,6 @@ setup(
         exclude=["build", "scripts", "dist", "images", "test_fixtures", "tests"]
     ),
     install_requires=["torch>=1.2.0"],
-    tests_require=["pytest", "pytest-cov", "PyYAML"],
     extras_require={"extras": ["PyYAML"]},
     python_requires=">=3.6",
     classifiers=[
