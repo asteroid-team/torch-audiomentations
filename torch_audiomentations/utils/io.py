36067e1120cc9f4da82aeeb5bd2fab4e29283c03
Hervé Bredin <bredin@limsi.fr>
2020-12-01 12:30:12 +0100

    fix: fix rounding error

--- torch_audiomentations/utils/io.py
+++ torch_audiomentations/utils/io.py
@@ -132,7 +132,7 @@ class Audio:
         else:
             num_samples, sample_rate = self.get_audio_metadata(file)
 
-        return math.floor(num_samples * self.sample_rate / sample_rate)
+        return round(num_samples * self.sample_rate / sample_rate)
 
     def __init__(self, sample_rate: int, mono: bool = True):
         super().__init__()
