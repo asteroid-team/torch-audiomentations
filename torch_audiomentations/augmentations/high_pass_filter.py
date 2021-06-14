b33c9087aa69e41af5ce254f237b56e8a9a6d2be
iver56 <iver56@hotmail.com>
2021-06-14 13:56:11 +0200

    Speed up the high pass filter by using julius instead of torchaudio

--- torch_audiomentations/augmentations/high_pass_filter.py
+++ torch_audiomentations/augmentations/high_pass_filter.py
@@ -1,10 +1,9 @@
 import torch
-from torchaudio.functional import highpass_biquad
 
-from ..core.transforms_interface import BaseWaveformTransform
+from ..augmentations.low_pass_filter import LowPassFilter
 
 
-class HighPassFilter(BaseWaveformTransform):
+class HighPassFilter(LowPassFilter):
     """
     Apply high-pass filtering to the input audio.
     """
@@ -12,62 +11,8 @@ class HighPassFilter(BaseWaveformTransform):
     supports_multichannel = True
     requires_sample_rate = True
 
-    def __init__(
-        self,
-        min_cutoff_freq=20,
-        max_cutoff_freq=500,
-        mode: str = "per_example",
-        p: float = 0.5,
-        p_mode: str = None,
-        sample_rate: int = None,
-    ):
-        """
-        :param min_cutoff_freq: Minimum cutoff frequency in hertz
-        :param max_cutoff_freq: Maximum cutoff frequency in hertz
-        :param mode:
-        :param p:
-        :param p_mode:
-        :param sample_rate:
-        """
-        super().__init__(mode, p, p_mode, sample_rate)
-
-        self.min_cutoff_freq = min_cutoff_freq
-        self.max_cutoff_freq = max_cutoff_freq
-        if self.min_cutoff_freq > self.max_cutoff_freq:
-            raise ValueError("min_cutoff_freq must not be greater than max_cutoff_freq")
-
-    def randomize_parameters(
-        self, selected_samples: torch.Tensor, sample_rate: int = None
-    ):
-        """
-        :params selected_samples: (batch_size, num_channels, num_samples)
-        """
-        batch_size, _, num_samples = selected_samples.shape
-
-        dist = torch.distributions.Uniform(
-            low=torch.tensor(
-                self.min_cutoff_freq, dtype=torch.float32, device=selected_samples.device
-            ),
-            high=torch.tensor(
-                self.max_cutoff_freq, dtype=torch.float32, device=selected_samples.device
-            ),
-            validate_args=True,
-        )
-        self.transform_parameters["cutoff_freq"] = dist.sample(sample_shape=(batch_size,))
-
     def apply_transform(self, selected_samples: torch.Tensor, sample_rate: int = None):
-        batch_size, num_channels, num_samples = selected_samples.shape
-
-        if sample_rate is None:
-            sample_rate = self.sample_rate
-
-        print(self.transform_parameters["cutoff_freq"])
-
-        for i in range(batch_size):
-            selected_samples[i] = highpass_biquad(
-                selected_samples[i],
-                sample_rate=sample_rate,
-                cutoff_freq=self.transform_parameters["cutoff_freq"][i],
-            )
-
-        return selected_samples
+        low_pass_filtered_samples = super().apply_transform(
+            selected_samples.clone(), sample_rate
+        )
+        return selected_samples - low_pass_filtered_samples
