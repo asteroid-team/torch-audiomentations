6c8e7fa13d28dbe4e9a8844f82f3a816973ca9cb
Kento Nishi <kento24gs@outlook.com>
2021-06-21 01:48:16 -0700

    writing code at 2am is clearly not very efficient

--- torch_audiomentations/augmentations/pitch_shift.py
+++ torch_audiomentations/augmentations/pitch_shift.py
@@ -62,12 +62,11 @@ class PitchShift(BaseWaveformTransform):
         if sample_rate is None:
             sample_rate = self._sample_rate
 
-        stacker = []
         for i in range(batch_size):
-            stacker.append(self._pitch_shift(
+            selected_samples[i, ...] = self._pitch_shift(
                 selected_samples[i],
                 self.transform_parameters["transpositions"][i],
                 sample_rate,
-            ))
+            )
 
-        return torch.stack(stacker)
+        return selected_samples
