2c7e995d55caf14afdd461fd491ec42f28980ab5
Kento Nishi <kento24gs@outlook.com>
2021-06-22 00:43:49 -0700

    properly get the device to run on

--- tests/test_pitch_shift.py
+++ tests/test_pitch_shift.py
@@ -7,39 +7,34 @@ from numpy.testing import assert_almost_equal
 from torch_audiomentations import PitchShift
 
 
-def get_example(device_name):
-    return torch.rand(size=(8, 2, 32000), dtype=torch.float32, device=device_name) - 0.5
+def get_example():
+    return (
+        torch.rand(
+            size=(8, 2, 32000),
+            dtype=torch.float32,
+            device="cuda" if torch.cuda.is_available() else "cpu",
+        )
+        - 0.5
+    )
 
 
 class TestPitchShift(unittest.TestCase):
-    @pytest.mark.parametrize(
-        "device_name",
-        [
-            pytest.param("cpu"),
-            pytest.param(
-                "cuda",
-                marks=pytest.mark.skip("Requires CUDA")
-                if not torch.cuda.is_available()
-                else [],
-            ),
-        ],
-    )
-    def test_per_batch_shift(self, device_name):
-        samples = get_example(device_name)
+    def test_per_batch_shift(self):
+        samples = get_example()
         aug = PitchShift(16000, p=1, mode="per_example")
         aug.randomize_parameters(samples)
         results = aug.apply_transform(samples)
         self.assertEqual(results.shape, samples.shape)
 
-    def test_per_example_shift(self, device_name):
-        samples = get_example(device_name)
+    def test_per_example_shift(self):
+        samples = get_example()
         aug = PitchShift(16000, p=1, mode="per_channel")
         aug.randomize_parameters(samples)
         results = aug.apply_transform(samples)
         self.assertEqual(results.shape, samples.shape)
 
-    def test_per_channel_shift(self, device_name):
-        samples = get_example(device_name)
+    def test_per_channel_shift(self):
+        samples = get_example()
         aug = PitchShift(16000, p=1, mode="per_batch")
         aug.randomize_parameters(samples)
         results = aug.apply_transform(samples)
