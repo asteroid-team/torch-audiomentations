a578cd55eb37846d1c67d1aa6c73f3b0adc5ad70
akashrajkn <akashrajkn@gmail.com>
2022-01-02 17:54:17 +0100

    Band stop filer - sanity unit test

---
+++ tests/test_band_stop_filter.py
@@ -0,0 +1,27 @@
+import unittest
+
+import numpy as np
+import torch
+
+from torch_audiomentations import BandStopFilter
+
+
+class TestBandStopFilter(unittest.TestCase):
+
+    def test_band_reject_filter(self):
+        samples = np.array(
+            [
+                [[0.75, 0.5, -0.25, -0.125, 0.0], [0.65, 0.5, -0.25, -0.125, 0.0]],
+                [[0.3, 0.5, -0.25, -0.125, 0.0], [0.9, 0.5, -0.25, -0.125, 0.0]],
+                [[0.9, 0.5, -0.25, -1.06, 0.0], [0.9, 0.5, -0.25, -1.12, 0.0]],
+            ],
+            dtype=np.float32,
+        )
+        sample_rate = 16000
+
+        augment = BandStopFilter(p=1.0)
+        processed_samples = augment(
+            samples=torch.from_numpy(samples), sample_rate=sample_rate
+        ).numpy()
+        self.assertEqual(processed_samples.shape, samples.shape)
+        self.assertEqual(processed_samples.dtype, np.float32)
