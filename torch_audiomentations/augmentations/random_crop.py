4769c56eac1058ea195db61bf95525e76880f1a6
Shahules786 <Shahules786@gmail.com>
2022-03-29 10:12:54 +0530

    type conversion

--- torch_audiomentations/augmentations/random_crop.py
+++ torch_audiomentations/augmentations/random_crop.py
@@ -12,7 +12,7 @@ class RandomCrop(torch.nn.Module):
     ):
         super(RandomCrop,self).__init__()
         self.sampling_rate = sampling_rate
-        self.num_samples = self.sampling_rate * seconds
+        self.num_samples = int(self.sampling_rate * seconds)
 
     
     def forward(self, samples, sampling_rate: typing.Optional[int] = None):
@@ -21,16 +21,16 @@ class RandomCrop(torch.nn.Module):
         if sample_rate is None:
             raise RuntimeError("sample_rate is required")
 
-        sample_length = samples.shape[2] / sample_rate
-        if sample_length < self.num_samples:
-            self.num_samples = sample_length
+        if samples.shape[2] < self.num_samples:
+            self.num_samples = samples.shape[2]
             raise RuntimeWarning("audio length less than cropping length")
             
         
-        start_indices = torch.randint(0,samples.shape[2] - self.num_samples,(sample_length.shape[0],))
+        start_indices = torch.randint(0,samples.shape[2] - self.num_samples,(samples.shape[2],))
         samples_cropped = torch.empty((samples.shape[0],samples.shape[1],self.num_samples))
         for i,sample in enumerate(samples):
-            samples_cropped[i] = sample[:,:,start_indices[i]:start_indices[i]+self.num_samples]
+            
+            samples_cropped[i] = sample.unsqueeze(0)[:,:,start_indices[i]:start_indices[i]+self.num_samples]
         
         return samples_cropped
 
