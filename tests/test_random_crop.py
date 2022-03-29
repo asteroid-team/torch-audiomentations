76a15874c29dc2b142c23318687d59c1e1a31644
Shahules786 <Shahules786@gmail.com>
2022-03-29 13:01:42 +0530

    cuda test for random crop

--- tests/test_random_crop.py
+++ tests/test_random_crop.py
@@ -1,10 +1,12 @@
 import unittest
 import torch
+import pytest
+import numpy as np
 from torch_audiomentations.augmentations.random_crop import RandomCrop
 
 class TestRandomCrop(unittest.TestCase):
     
-    def testcrop(self):
+    def test_crop(self):
         samples =  torch.rand(size=(8, 2, 32000), dtype=torch.float32)
         sampling_rate = 16000
         crop_to = 1.5
@@ -13,6 +15,31 @@ class TestRandomCrop(unittest.TestCase):
         cropped_samples = Crop(samples)
         
         self.assertEqual(desired_samples_len, cropped_samples.size(-1))
+
+    def test_crop_larger_cropto(self):
+        samples =  torch.rand(size=(8, 2, 32000), dtype=torch.float32)
+        sampling_rate = 16000
+        crop_to = 3
+        Crop = RandomCrop(seconds=crop_to,sampling_rate=sampling_rate)
+        cropped_samples = Crop(samples)
+        
+        np.testing.assert_array_equal(samples, cropped_samples)
+        self.assertEqual(samples.size(-1), cropped_samples.size(-1))
+
+    @pytest.mark.skipif(not torch.cuda.is_available(), reason="Requires CUDA")
+    def test_crop_on_device_cuda(self):
+
+        samples =  torch.rand(size=(8, 2, 32000), dtype=torch.float32,device=torch.device('cuda'))
+        sampling_rate = 16000
+        crop_to = 1.5
+        desired_samples_len = sampling_rate*crop_to 
+        Crop = RandomCrop(seconds=crop_to,sampling_rate=sampling_rate)
+        cropped_samples = Crop(samples)
+        
+        self.assertEqual(desired_samples_len, cropped_samples.size(-1))
+
+
+
 
         
 
