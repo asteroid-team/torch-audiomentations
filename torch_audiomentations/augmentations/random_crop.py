c9a8e10b1cdebbcc555fd10d5ad36b6f223b9d97
Shahules786 <Shahules786@gmail.com>
2022-03-29 10:23:56 +0530

    input sample checks

--- torch_audiomentations/augmentations/random_crop.py
+++ torch_audiomentations/augmentations/random_crop.py
@@ -1,9 +1,13 @@
 import torch
 import typing
+import warnings
+from torch_audiomentations.utils.multichannel import is_multichannel
+from ..core.transforms_interface import MultichannelAudioNotSupportedException
 
 class RandomCrop(torch.nn.Module):
 
     requires_sample_rate = True
+    supports_multichannel = True
 
     def __init__(
         self,
@@ -20,6 +24,34 @@ class RandomCrop(torch.nn.Module):
         sample_rate = sampling_rate or self.sampling_rate
         if sample_rate is None:
             raise RuntimeError("sample_rate is required")
+
+        if len(samples) == 0:
+            warnings.warn(
+                "An empty samples tensor was passed to {}".format(self.__class__.__name__)
+            )
+            return samples
+
+        if len(samples.shape) != 3:
+            raise RuntimeError(
+                "torch-audiomentations expects input tensors to be three-dimensional, with"
+                " dimension ordering like [batch_size, num_channels, num_samples]. If your"
+                " audio is mono, you can use a shape like [batch_size, 1, num_samples]."
+            )
+
+        if is_multichannel(samples):
+            if samples.shape[1] > samples.shape[2]:
+                warnings.warn(
+                    "Multichannel audio must have channels first, not channels last. In"
+                    " other words, the shape must be (batch size, channels, samples), not"
+                    " (batch_size, samples, channels)"
+                )
+            if not self.supports_multichannel:
+                raise MultichannelAudioNotSupportedException(
+                    "{} only supports mono audio, not multichannel audio".format(
+                        self.__class__.__name__
+                    )
+                )
+
 
         if samples.shape[2] < self.num_samples:
             self.num_samples = samples.shape[2]
