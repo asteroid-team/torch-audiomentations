52f26da3353d3180e8931784072edec4511e7748
iver56 <iver56@hotmail.com>
2022-09-27 12:25:59 +0200

    wip

--- torch_audiomentations/augmentations/random_crop.py
+++ torch_audiomentations/augmentations/random_crop.py
@@ -32,6 +32,8 @@ class RandomCrop(torch.nn.Module):
         self.output_length = output_length
         self.output_length_unit = output_length_unit
         self.sample_rate = sample_rate
+        self.transform_parameters = {}
+        self.are_parameters_frozen = False  # TODO: Actually use this
 
     def randomize_parameters(
         self,
@@ -40,24 +42,6 @@ class RandomCrop(torch.nn.Module):
         targets: Optional[torch.Tensor] = None,
         target_rate: Optional[int] = None,
     ):
-        if self.output_length_unit == "seconds":
-            num_samples = int(self.sample_rate * self.output_length)
-
-            sample_rate = sample_rate or self.sample_rate
-            if sample_rate is None:
-                raise RuntimeError("sample_rate is required")
-        elif self.output_length_unit == "samples":
-            num_samples = int(self.output_length)
-        else:
-            raise ValueError('output_length_unit must be "samples" or "seconds"')
-
-    def forward(self, samples, sample_rate: Optional[int] = None):
-        if len(samples) == 0:
-            warnings.warn(
-                "An empty samples tensor was passed to {}".format(self.__class__.__name__)
-            )
-            return samples
-
         if len(samples.shape) != 3:
             raise RuntimeError(
                 "torch-audiomentations expects input tensors to be three-dimensional, with"
@@ -79,20 +63,45 @@ class RandomCrop(torch.nn.Module):
                     )
                 )
 
-        if samples.shape[2] < self.num_samples:
+        if self.output_length_unit == "seconds":
+            sample_rate = sample_rate or self.sample_rate
+            if sample_rate is None:
+                raise RuntimeError("sample_rate is required")
+            self.transform_parameters["num_samples"] = int(
+                sample_rate * self.output_length
+            )
+        elif self.output_length_unit == "samples":
+            self.transform_parameters["num_samples"] = int(self.output_length)
+        else:
+            raise ValueError('output_length_unit must be "samples" or "seconds"')
+
+    def apply_transform(self, samples, sample_rate: Optional[int] = None):
+        if len(samples) == 0:
+            warnings.warn(
+                "An empty samples tensor was passed to {}".format(self.__class__.__name__)
+            )
+            return samples
+
+        if samples.shape[-1] < self.num_samples:
             warnings.warn("audio length less than cropping length")
+            # TODO: We need to apply padding here
+            # TODO: Pad only end? Or place the audio randomly in the alloted placeholder?
             return samples
 
         start_indices = torch.randint(
             0, samples.shape[2] - self.num_samples, (samples.shape[2],)
         )
+
         samples_cropped = torch.empty(
             (samples.shape[0], samples.shape[1], self.num_samples), device=samples.device
         )
         for i, sample in enumerate(samples):
-
             samples_cropped[i] = sample.unsqueeze(0)[
                 :, :, start_indices[i] : start_indices[i] + self.num_samples
             ]
 
         return samples_cropped
+
+    def forward(self, samples, sample_rate: Optional[int] = None):
+        self.randomize_parameters(samples, sample_rate)
+        return self.apply_transform(samples, sample_rate)
